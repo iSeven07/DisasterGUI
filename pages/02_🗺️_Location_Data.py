fcb9605294e81cdeeb699fafbc423abb4d71e4d6
Joe Auz <joseph.auz77@gmail.com>
2023-04-06 19:02:50 -0500

    refactor of location data page

--- pages/02_🗺️_Location_Data.py
+++ pages/02_🗺️_Location_Data.py
@@ -1,28 +1,49 @@
 import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
-
+# Sets browser tab information
 st.set_page_config(page_title="NDD - Location Data",
                    page_icon="🗺️", layout="wide")
 
+# Adds title to top of page
 st.title("🗺️ Disaster Locations Dashboard")
 
+# Reads in the csv file and limits the number of rows it reads to 10000
 df = pd.read_csv('locations_for_storm_events.csv', nrows=10000)
 
-scatter_map = go.Figure(go.Scattergeo(lat=df['LATITUDE'], lon=df['LONGITUDE']))
-scatter_map.update_layout(
-    title='Disasters Latitude / Longitude Location',
-    geo=dict(
-        scope='usa',
-        showsubunits=True,
-        center=dict(lat=37.0902, lon=-95.7129),
-        projection=dict(type='albers usa'), #equirectangular
-        showcountries=False,
-    )
-)
+def scatter_map(df):
+  # Creation of the scatter plot map [SCATTER PLOT MAP]
+  scatter_map = go.Figure(go.Scattergeo(lat=df['LATITUDE'], lon=df['LONGITUDE'], mode='markers', marker=dict(size=1.5, line=dict(width=1, color='red'))))
 
-map_container = st.container()
-map_container.plotly_chart(scatter_map, use_container_width=True)
+  # Refines the scope of the scatter plot map [SCATTER PLOT MAP]
+  scatter_map.update_layout(
+      title='Disasters Latitude / Longitude Location',
+      geo=dict(
+          scope='usa',
+          showsubunits=True,
+          subunitcolor='black',
+          subunitwidth=2,
+          center=dict(lat=37.0902, lon=-95.7129),
+          projection=dict(type='albers usa'), #equirectangular
+          showcountries=False,
+
+      )
+  )
+
+  # Customizes the map to add lakes and rivers [SCATTER PLOT MAP]
+  scatter_map.update_geos(
+      resolution=50,
+      showlakes=True,
+      lakecolor='Blue',
+      showrivers=True,
+      rivercolor='Blue',
+  )
+
+  # Creates a container on the page and displays the map [SCATTER PLOT MAP]
+  map_container = st.container()
+  map_container.plotly_chart(scatter_map, use_container_width=True)
+
+scatter_map(df)
 
 # ---- STREAMLIT STYLE ----
 st_style = """
